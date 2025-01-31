f69bf8cffba15fae88cdc63cbe7d6abee6e8f228
husen <iamhusen101@gmail.com>
2025-01-31 15:33:10 +0530

    fix notification issues

---
+++ lib/constants/mini_consts.dart
@@ -0,0 +1,9 @@
+
+
+//Preference constants
+///Whether this is first time notification permission is requested
+const mFirstTimeNotifPermission = 'first_time_notif';
+///Whether the notification channels initialized or not
+const mChannelsInit = 'channels_init';
+///Whether or not the notifications are enabled
+const mNotificationsEnabled = 'notifications_enabled';
