7b2b868588d295e170f69d887e9148385e34b8cd
husen <iamhusen101@gmail.com>
2025-05-07 23:22:27 +0530

    add sliver like scrolling functionality in tasks page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get_storage/get_storage.dart';
