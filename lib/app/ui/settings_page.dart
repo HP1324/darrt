0899db9e54f2363de89e428a907cf71a5e95d213
husen <iamhusen101@gmail.com>
2025-06-21 12:21:10 +0530

    initiate implementing drive backup feature

--- lib/app/ui/settings_page.dart
+++ lib/app/ui/settings_page.dart
@@ -1,6 +1,4 @@
-import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
-import 'package:minimaltodo/app/setting/settings_service.dart';
 import 'package:minimaltodo/app/state/value_notifiers.dart';
 import 'package:minimaltodo/task/ui/add_task_page.dart';
 
@@ -18,13 +16,51 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(title: const Text('Settings'), centerTitle: true),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          spacing: 10,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DefaultReminderTypeSection(),
-            SnoozeSection(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 18,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DefaultReminderTypeSection(),
+              Divider(),
+              SnoozeSection(),
+              Divider(),
+              Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surfaceContainerHigh,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  spacing: -1,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Backup & Restore', style: Theme.of(context).textTheme.titleMedium),
+                    Divider(height: 0),
+                    ListTile(
+                      visualDensity: VisualDensity.compact,
+                      onTap: (){},
+                      contentPadding: EdgeInsets.zero,
+                      title: Text('Google Account'),
+                      subtitle: Text('No account set'),
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.zero,
+                      visualDensity: VisualDensity.compact,
+                      value: true,
+                      onChanged: (value) {},
+                      title: Text('Auto backup'),
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      visualDensity: VisualDensity.compact,
+                      title: Text('Restore data'),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -37,41 +73,41 @@ class SnoozeSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return StructuredRow(
-      leadingIcon: Icons.snooze,
-      expanded: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text('Snooze for (in minutes)',style: theme.textTheme.titleMedium,),
-            ],
-          ),
-          SizedBox(height: 8),
-          Wrap(
-            spacing: 8,
-            runSpacing: 4,
-            children: [1,5, 10, 15, 20, 25, 30, 45,50,60].map((minutes) {
-              return ValueListenableBuilder(
-                valueListenable: snoozeMinutes,
-                builder: (context, value, child) {
-                  return ChoiceChip(
-                    showCheckmark: false,
-                    shape: StadiumBorder(),
-                    label: Text('$minutes'),
-                    selected: value == minutes,
-                    onSelected: (selected) {
-                      if (selected) {
-                        updateSnoozeMinutes(minutes);
-                      }
-                    },
-                  );
-                },
-              );
-            }).toList(),
-          ),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(
+              'Snooze for (in minutes)',
+              style: theme.textTheme.titleMedium,
+            ),
+          ],
+        ),
+        SizedBox(height: 8),
+        Wrap(
+          spacing: 8,
+          runSpacing: 4,
+          children: [1, 5, 10, 15, 20, 25, 30, 45, 50, 60].map((minutes) {
+            return ValueListenableBuilder(
+              valueListenable: snoozeMinutes,
+              builder: (context, value, child) {
+                return ChoiceChip(
+                  showCheckmark: false,
+                  shape: StadiumBorder(),
+                  label: Text('$minutes'),
+                  selected: value == minutes,
+                  onSelected: (selected) {
+                    if (selected) {
+                      updateSnoozeMinutes(minutes);
+                    }
+                  },
+                );
+              },
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
@@ -82,61 +118,35 @@ class DefaultReminderTypeSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return StructuredRow(
-      leadingIcon: Icons.event_note,
-      expanded: Column(
-        children: [
-          Row(
-            children: [
-              Text('Default reminder type', style: theme.textTheme.titleMedium),
-            ],
-          ),
-          ValueListenableBuilder(
-            valueListenable: defaultReminderType,
-            builder: (context, value, child) {
-              return SegmentedButton<String>(
-                segments: const [
-                  ButtonSegment(
-                    value: 'notif',
-                    label: Text('Notification'),
-                    icon: Icon(Icons.notifications),
-                  ),
-                  ButtonSegment(
-                    value: 'alarm',
-                    label: Text('Alarm'),
-                    icon: Icon(Icons.alarm),
-                  ),
-                ],
-                selected: {value},
-                onSelectionChanged: (Set<String> selection) {
-                  updateDefaultReminder(selection.first);
-                },
-              );
-            },
-          ),
-        ],
-      ),
+    return Column(
+      spacing: 10,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Default reminder type', style: theme.textTheme.titleMedium),
+        ValueListenableBuilder(
+          valueListenable: defaultReminderType,
+          builder: (context, value, child) {
+            return SegmentedButton<String>(
+              segments: const [
+                ButtonSegment(
+                  value: 'notif',
+                  label: Text('Notification'),
+                  icon: Icon(Icons.notifications),
+                ),
+                ButtonSegment(
+                  value: 'alarm',
+                  label: Text('Alarm'),
+                  icon: Icon(Icons.alarm),
+                ),
+              ],
+              selected: {value},
+              onSelectionChanged: (Set<String> selection) {
+                updateDefaultReminder(selection.first);
+              },
+            );
+          },
+        ),
+      ],
     );
-  }
-}
-
-class SettingsItem extends StatelessWidget {
-  const SettingsItem({
-    super.key,
-    required this.leading,
-    required this.title,
-    required this.subtitle,
-    this.trailing,
-  });
-  final Widget leading;
-
-  final Widget title;
-
-  final Widget subtitle;
-
-  final Widget? trailing;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(leading: leading, title: title, subtitle: subtitle, trailing: trailing);
   }
 }
