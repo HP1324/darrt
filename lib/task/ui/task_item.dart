9cbe4b93ddab0bcdb64f5a377116abd787483904
husen <iamhusen101@gmail.com>
2025-07-05 18:07:05 +0530

    improve organization task item widget trees

--- lib/task/ui/task_item.dart
+++ lib/task/ui/task_item.dart
@@ -18,91 +18,193 @@ class TaskItem extends StatefulWidget {
 class _TaskItemState extends State<TaskItem> {
   @override
   Widget build(BuildContext context) {
-  MiniLogger.dp('Task: title: ${widget.task.title}, id: ${widget.task.id}');
-    final scheme = Theme.of(context).colorScheme;
+    MiniLogger.dp('Task: title: ${widget.task.title}, id: ${widget.task.id}');
     return ListenableBuilder(
       listenable: g.taskVm,
       builder: (context, child) {
         final ids = g.taskVm.selectedTaskIds;
         final isSelected = ids.contains(widget.task.id);
-        final isUrgent = widget.task.priority.toLowerCase() == 'urgent';
 
-        return Container(
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-          decoration: BoxDecoration(
-            color: isSelected ? scheme.outline.withAlpha(130) : scheme.surface.withAlpha(100),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              color: scheme.outline.withAlpha(60),
-              width: 0.5,
-            ),
-          ),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              splashFactory: NoSplash.splashFactory,
-              borderRadius: BorderRadius.circular(12),
-              onTap: () {
-                if (ids.isEmpty) {
-                  MiniRouter.to(context, AddTaskPage(edit: true, task: widget.task));
-                } else {
-                  g.taskVm.toggleSelection(widget.task.id);
-                }
-              },
-              onLongPress: () {
-                g.taskVm.toggleSelection(widget.task.id);
-              },
-              child: SizedBox(
-                height: 72,
-                child: Row(
-                  children: [
-                    TaskFinishCheckbox(widget: widget),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 12, 10, 12),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              spacing: 8.0,
-                              children: [
-                                TaskTitle(widget: widget),
-                                // TaskPrioritySection(widget: widget, isUrgent: isUrgent, context: context),
-                              ],
-                            ),
-                            const Spacer(),
-                            // Info row
-                            Row(
-                              children: [
-                                TaskCategoriesSection(widget: widget),
-                                // if (!widget.task.isRepeating) ...[
-                                //   Text(
-                                //     DateFormat('MMM d').format(widget.task.dueDate),
-                                //     style: Theme.of(context).textTheme.labelSmall,
-                                //   ),
-                                //   const SizedBox(width: 4),
-                                // ],
-                                if (widget.task.isRepeating) ...[
-                                  const SizedBox(width: 4),
-                                  Icon(
-                                    FontAwesomeIcons.repeat,
-                                    size: 13,
-                                    color: Theme.of(context).colorScheme.primary.withAlpha(200),
-                                  ),
-                                ],
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+        return TaskItemContainer(
+          task: widget.task,
+          isSelected: isSelected,
+          onTap: () => _handleTap(ids),
+          onLongPress: () => g.taskVm.toggleSelection(widget.task.id),
         );
       },
+    );
+  }
+
+  void _handleTap(Set<int> selectedIds) {
+    if (selectedIds.isEmpty) {
+      MiniRouter.to(context, AddTaskPage(edit: true, task: widget.task));
+    } else {
+      g.taskVm.toggleSelection(widget.task.id);
+    }
+  }
+}
+
+class TaskItemContainer extends StatelessWidget {
+  const TaskItemContainer({
+    super.key,
+    required this.task,
+    required this.isSelected,
+    required this.onTap,
+    required this.onLongPress,
+  });
+
+  final Task task;
+  final bool isSelected;
+  final VoidCallback onTap;
+  final VoidCallback onLongPress;
+
+  @override
+  Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      decoration: BoxDecoration(
+        color: isSelected ? scheme.outline.withAlpha(130) : scheme.surface.withAlpha(100),
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: scheme.outline.withAlpha(60),
+          width: 0.5,
+        ),
+      ),
+      child: TaskItemInkWell(
+        task: task,
+        onTap: onTap,
+        onLongPress: onLongPress,
+      ),
+    );
+  }
+}
+
+class TaskItemInkWell extends StatelessWidget {
+  const TaskItemInkWell({
+    super.key,
+    required this.task,
+    required this.onTap,
+    required this.onLongPress,
+  });
+
+  final Task task;
+  final VoidCallback onTap;
+  final VoidCallback onLongPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        splashFactory: NoSplash.splashFactory,
+        borderRadius: BorderRadius.circular(12),
+        onTap: onTap,
+        onLongPress: onLongPress,
+        child: TaskItemContent(task: task),
+      ),
+    );
+  }
+}
+
+class TaskItemContent extends StatelessWidget {
+  const TaskItemContent({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 72,
+      child: Row(
+        children: [
+          TaskFinishCheckbox(task: task),
+          Expanded(
+            child: TaskItemDetails(task: task),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TaskItemDetails extends StatelessWidget {
+  const TaskItemDetails({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 12, 10, 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TaskTitleRow(task: task),
+          const Spacer(),
+          TaskInfoRow(task: task),
+        ],
+      ),
+    );
+  }
+}
+
+class TaskTitleRow extends StatelessWidget {
+  const TaskTitleRow({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 8.0,
+      children: [
+        TaskTitle(task: task),
+        // TaskPrioritySection(task: task, isUrgent: isUrgent, context: context),
+      ],
+    );
+  }
+}
+
+class TaskInfoRow extends StatelessWidget {
+  const TaskInfoRow({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        TaskCategoriesSection(task: task),
+        // if (!task.isRepeating) ...[
+        //   Text(
+        //     DateFormat('MMM d').format(task.dueDate),
+        //     style: Theme.of(context).textTheme.labelSmall,
+        //   ),
+        //   const SizedBox(width: 4),
+        // ],
+        if (task.isRepeating) ...[
+          const SizedBox(width: 4),
+          Icon(
+            FontAwesomeIcons.repeat,
+            size: 13,
+            color: Theme.of(context).colorScheme.primary.withAlpha(200),
+          ),
+        ],
+      ],
     );
   }
 }
@@ -110,10 +212,10 @@ class _TaskItemState extends State<TaskItem> {
 class TaskTitle extends StatelessWidget {
   const TaskTitle({
     super.key,
-    required this.widget,
+    required this.task,
   });
 
-  final TaskItem widget;
+  final Task task;
 
   @override
   Widget build(BuildContext context) {
@@ -122,14 +224,14 @@ class TaskTitle extends StatelessWidget {
           listenable: Listenable.merge([g.calMan, g.taskVm]),
           builder: (context, child) {
             final date = DateUtils.dateOnly(g.calMan.selectedDate).millisecondsSinceEpoch;
-            final repeat = widget.task.isRepeating;
+            final repeat = task.isRepeating;
             final stc = g.taskVm.onetimeTaskCompletions, rtc = g.taskVm.repeatingTaskCompletions;
             final isFinished = repeat
-                ? rtc[widget.task.id]?.contains(date) ?? false
-                : stc[widget.task.id] ?? false;
+                ? rtc[task.id]?.contains(date) ?? false
+                : stc[task.id] ?? false;
 
             return Text(
-              widget.task.title,
+              task.title,
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
               style: TextStyle(
@@ -150,14 +252,12 @@ class TaskTitle extends StatelessWidget {
 class TaskPrioritySection extends StatelessWidget {
   const TaskPrioritySection({
     super.key,
-    required this.widget,
+    required this.task,
     required this.isUrgent,
-    required this.context,
   });
 
-  final TaskItem widget;
+  final Task task;
   final bool isUrgent;
-  final BuildContext context;
 
   @override
   Widget build(BuildContext context) {
@@ -171,7 +271,7 @@ class TaskPrioritySection extends StatelessWidget {
         ),
         const SizedBox(width: 4),
         Text(
-          widget.task.priority,
+          task.priority,
           style: TextStyle(
             fontSize: Theme.of(context).textTheme.labelSmall!.fontSize,
             color: isUrgent ? Colors.red.shade400 : null,
@@ -185,10 +285,10 @@ class TaskPrioritySection extends StatelessWidget {
 class TaskCategoriesSection extends StatelessWidget {
   const TaskCategoriesSection({
     super.key,
-    required this.widget,
+    required this.task,
   });
 
-  final TaskItem widget;
+  final Task task;
 
   @override
   Widget build(BuildContext context) {
@@ -205,31 +305,45 @@ class TaskCategoriesSection extends StatelessWidget {
             ).createShader(bounds);
           },
           blendMode: BlendMode.dstIn,
-          child: ListenableBuilder(
-            listenable: g.catVm,
-            builder: (context, child) {
-              var categories = g.catVm.categories;
-              widget.task.categories.removeWhere((c) => !categories.contains(c));
-              if (widget.task.categories.isEmpty) {
-                MiniLogger.dp('This condition called');
-                widget.task.categories.add(TaskCategory(id: 1, name: 'General'));
-                widget.task.categories.applyToDb();
-              }
-              return ListView.separated(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                physics: BouncingScrollPhysics(),
-                separatorBuilder: (context, index) => const SizedBox(width: 2),
-                itemCount: widget.task.categories.length,
-                itemBuilder: (context, index) {
-                  final category = widget.task.categories[index];
-                  return CategoryChip(category: category);
-                },
-              );
-            },
-          ),
+          child: TaskCategoriesList(task: task),
         ),
       ),
+    );
+  }
+}
+
+class TaskCategoriesList extends StatelessWidget {
+  const TaskCategoriesList({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: g.catVm,
+      builder: (context, child) {
+        var categories = g.catVm.categories;
+        task.categories.removeWhere((c) => !categories.contains(c));
+        if (task.categories.isEmpty) {
+          MiniLogger.dp('This condition called');
+          task.categories.add(TaskCategory(id: 1, name: 'General'));
+          task.categories.applyToDb();
+        }
+        return ListView.separated(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
+          separatorBuilder: (context, index) => const SizedBox(width: 2),
+          itemCount: task.categories.length,
+          itemBuilder: (context, index) {
+            final category = task.categories[index];
+            return CategoryChip(category: category);
+          },
+        );
+      },
     );
   }
 }
@@ -237,10 +351,10 @@ class TaskCategoriesSection extends StatelessWidget {
 class TaskFinishCheckbox extends StatelessWidget {
   const TaskFinishCheckbox({
     super.key,
-    required this.widget,
+    required this.task,
   });
 
-  final TaskItem widget;
+  final Task task;
 
   @override
   Widget build(BuildContext context) {
@@ -251,74 +365,70 @@ class TaskFinishCheckbox extends StatelessWidget {
         child: ListenableBuilder(
           listenable: Listenable.merge([g.taskVm, g.calMan]),
           builder: (context, child) {
-            final repeat = widget.task.isRepeating;
+            final repeat = task.isRepeating;
             final oneTimeCompletions = g.taskVm.onetimeTaskCompletions;
             final repeatingCompletions = g.taskVm.repeatingTaskCompletions;
             final date = DateUtils.dateOnly(g.calMan.selectedDate).millisecondsSinceEpoch;
-            final colorScheme = Theme.of(context).colorScheme;
+
             return CheckboxTheme(
-              data: CheckboxThemeData(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                ),
-                side: WidgetStateBorderSide.resolveWith((states) {
-                  if (states.contains(WidgetState.selected)) {
-                    return BorderSide(
-                      color: colorScheme.secondary,
-                      width: 2.5,
-                    );
-                  }
-                  if (states.contains(WidgetState.hovered)) {
-                    return BorderSide(
-                      color: colorScheme.secondary.withAlpha(160),
-                      width: 2.5,
-                    );
-                  }
-                  return BorderSide(
-                    color: colorScheme.outline,
-                    width: 2.0,
-                  );
-                }),
-                fillColor: WidgetStateProperty.resolveWith<Color>((states) {
-                  if (states.contains(WidgetState.selected)) {
-                    return colorScheme.secondary;
-                  }
-                  if (states.contains(WidgetState.hovered)) {
-                    return colorScheme.secondary.withAlpha(40);
-                  }
-                  return colorScheme.surface;
-                }),
-                overlayColor: WidgetStateProperty.resolveWith<Color>((states) {
-                  if (states.contains(WidgetState.pressed)) {
-                    return colorScheme.secondary.withAlpha(100);
-                  }
-                  if (states.contains(WidgetState.hovered)) {
-                    return colorScheme.secondary.withAlpha(50);
-                  }
-                  return Colors.transparent;
-                }),
-                checkColor: WidgetStateProperty.resolveWith<Color>((states) {
-                  if (states.contains(WidgetState.selected)) {
-                    return colorScheme.onSecondary;
-                  }
-                  return colorScheme.onSurface;
-                }),
-                splashRadius: 22,
-              ),
+              data: _buildCheckboxTheme(context),
               child: Checkbox(
-                key: ValueKey('selection_${widget.task.id}'),
-                // shape: CircleBorder(),
+                key: ValueKey('selection_${task.id}'),
                 value: repeat
-                    ? repeatingCompletions[widget.task.id]?.contains(date) ?? false
-                    : oneTimeCompletions[widget.task.id] ?? false,
+                    ? repeatingCompletions[task.id]?.contains(date) ?? false
+                    : oneTimeCompletions[task.id] ?? false,
                 onChanged: (value) {
-                  g.taskVm.toggleStatus(widget.task, value ?? false, g.calMan.selectedDate);
+                  g.taskVm.toggleStatus(task, value ?? false, g.calMan.selectedDate);
                 },
               ),
             );
           },
         ),
       ),
+    );
+  }
+
+  CheckboxThemeData _buildCheckboxTheme(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return CheckboxThemeData(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      ),
+      side: WidgetStateBorderSide.resolveWith((states) {
+        if (states.contains(WidgetState.selected)) {
+          return BorderSide(color: colorScheme.secondary, width: 2.5);
+        }
+        if (states.contains(WidgetState.hovered)) {
+          return BorderSide(color: colorScheme.secondary.withAlpha(160), width: 2.5);
+        }
+        return BorderSide(color: colorScheme.outline, width: 2.0);
+      }),
+      fillColor: WidgetStateProperty.resolveWith<Color>((states) {
+        if (states.contains(WidgetState.selected)) {
+          return colorScheme.secondary;
+        }
+        if (states.contains(WidgetState.hovered)) {
+          return colorScheme.secondary.withAlpha(40);
+        }
+        return colorScheme.surface;
+      }),
+      overlayColor: WidgetStateProperty.resolveWith<Color>((states) {
+        if (states.contains(WidgetState.pressed)) {
+          return colorScheme.secondary.withAlpha(100);
+        }
+        if (states.contains(WidgetState.hovered)) {
+          return colorScheme.secondary.withAlpha(50);
+        }
+        return Colors.transparent;
+      }),
+      checkColor: WidgetStateProperty.resolveWith<Color>((states) {
+        if (states.contains(WidgetState.selected)) {
+          return colorScheme.onSecondary;
+        }
+        return colorScheme.onSurface;
+      }),
+      splashRadius: 22,
     );
   }
 }
