ed7523d37b3c8f224988990d978872867ad81cbd
husen <iamhusen101@gmail.com>
2025-07-16 14:56:58 +0530

    prepare stats model

--- lib/task/statistics/task_stats.dart
+++ lib/task/statistics/task_stats.dart
@@ -1,13 +1,53 @@
+import 'dart:convert';
 class TaskStats {
   List<DateTime> completions;
-
   DateTime? currentStreakStart;
-
   int currentStreakLength;
 
-
   TaskStats({
-    this.completions =const [],
+    this.completions = const [],
     this.currentStreakLength = 0,
+    this.currentStreakStart,
   });
+
+  TaskStats copyWith({
+    List<DateTime>? completions,
+    DateTime? currentStreakStart,
+    int? currentStreakLength,
+  }) {
+    return TaskStats(
+      completions: completions ?? this.completions,
+      currentStreakStart: currentStreakStart ?? this.currentStreakStart,
+      currentStreakLength: currentStreakLength ?? this.currentStreakLength,
+    );
+  }
+
+  /// Convert the object to a JSON string
+  String toJsonString() {
+    final map = {
+      'completions': completions.map((d) => d.millisecondsSinceEpoch).toList(),
+      'currentStreakStart': currentStreakStart?.millisecondsSinceEpoch,
+      'currentStreakLength': currentStreakLength,
+    };
+    return jsonEncode(map);
+  }
+
+  /// Create an object from a JSON string
+  static TaskStats? fromJsonString(String jsonString) {
+    try {
+      final map = jsonDecode(jsonString);
+      return TaskStats(
+        completions: (map['completions'] as List<int>)
+            .map((d) => DateTime.fromMillisecondsSinceEpoch(d))
+            .toList(),
+        currentStreakStart: map['currentStreakStart'] != null
+            ? DateTime.fromMillisecondsSinceEpoch(map['currentStreakStart'])
+            : null,
+        currentStreakLength: map['currentStreakLength'] ?? 0,
+      );
+    } catch (e) {
+      // handle malformed or corrupt json safely
+      return null;
+    }
+  }
 }
