530184e022648cf5561ac792d0972e29ab348d39
husen <iamhusen101@gmail.com>
2025-07-16 15:20:03 +0530

    wip: task stats

--- lib/task/statistics/stats_page.dart
+++ lib/task/statistics/stats_page.dart
@@ -2,6 +2,7 @@ import 'package:darrt/task/models/task.dart';
 import 'package:darrt/task/statistics/stats_calendar_widget.dart';
 import 'package:flutter/material.dart';
 
+import '../../helpers/globals.dart' as g show taskVm;
 class StatsPage extends StatefulWidget {
   const StatsPage({super.key, required this.task});
   final Task task;
@@ -10,6 +11,11 @@ class StatsPage extends StatefulWidget {
 }
 
 class _StatsPageState extends State<StatsPage> {
+  @override
+  void initState() {
+    super.initState();
+    g.taskVm.initTaskStats(widget.task);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
