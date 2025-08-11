b5e78991e9c875e5148460b88ccb631e5dc2bc02
husen <iamhusen101@gmail.com>
2025-08-11 19:09:10 +0530

    unnecessary build runner changes

--- lib/app/ui/settings_page/backup_provider.g.dart
+++ lib/app/ui/settings_page/backup_provider.g.dart
@@ -6,7 +6,7 @@ part of 'backup_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$backupNotifierHash() => r'9b12aea110e7cbcde54f96d767dc3e7af03b7017';
+String _$backupNotifierHash() => r'8baffe77f108e9bbe1d312e277465129bb922973';
 
 /// See also [BackupNotifier].
 @ProviderFor(BackupNotifier)
