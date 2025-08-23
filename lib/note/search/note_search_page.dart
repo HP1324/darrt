2e94c44f91c082c70e34a2f16a5b6f9e56c115dc
husen <iamhusen101@gmail.com>
2025-08-23 22:16:47 +0530

    fix: use new sliver grid strucutre

--- lib/note/search/note_search_page.dart
+++ lib/note/search/note_search_page.dart
@@ -6,7 +6,6 @@ import 'package:darrt/note/models/note.dart';
 import 'package:darrt/note/ui/note_item.dart';
 import 'package:darrt/objectbox.g.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class NoteSearchPage extends StatefulWidget {
   const NoteSearchPage({super.key});
@@ -16,72 +15,78 @@ class NoteSearchPage extends StatefulWidget {
 }
 
 class _NoteSearchPageState extends State<NoteSearchPage> {
-  late final TextEditingController _searchController;
+  late final TextEditingController searchController;
 
-  late final StreamController<String> _streamController;
+  late final StreamController<String> searchStreamController;
 
   @override
   void initState() {
     super.initState();
-    _searchController = TextEditingController();
-    _streamController = StreamController<String>();
+    searchController = TextEditingController();
+    searchStreamController = StreamController<String>();
   }
 
   @override
   void dispose() {
-    _searchController.dispose();
-    _streamController.close();
+    searchController.dispose();
+    searchStreamController.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            leading: BackButton(),
-            titleSpacing: 0,
-            title: TextField(
-              controller: _searchController,
-              autofocus: true,
-              decoration: InputDecoration(
-                hintText: 'Search notes...',
-                border: InputBorder.none,
+    return Padding(
+        padding: EdgeInsets.all(10),
+      child: Material(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: BackButton(),
+              titleSpacing: 0,
+              title: TextField(
+                controller: searchController,
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: 'Search notes...',
+                  border: InputBorder.none,
+                ),
+                onChanged: (value) {
+                  searchStreamController.sink.add(value);
+                },
               ),
-              onChanged: (value) {
-                _streamController.sink.add(value);
+            ),
+            StreamBuilder(
+              stream: searchStreamController.stream,
+              builder: (context, snapshot) {
+                final searchQuery = snapshot.data ?? '';
+
+                final box = ObjectBox().noteBox;
+
+                final List<Note> foundNotes = box
+                    .query(Note_.content.contains(searchQuery, caseSensitive: false))
+                    .build()
+                    .find();
+
+                if (foundNotes.isNotEmpty) {
+                  return SliverGrid.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                        mainAxisExtent: MediaQuery.sizeOf(context).height * 0.16
+                    ),
+                    itemCount: foundNotes.length,
+                    itemBuilder: (context, index) {
+                      final note = foundNotes[index];
+                      return NoteItem(note: note);
+                    },
+                  );
+                }
+                return _NotesEmptyIndicator();
               },
             ),
-          ),
-          StreamBuilder(
-            stream: _streamController.stream,
-            builder: (context, snapshot) {
-              final searchQuery = snapshot.data ?? '';
-              
-              final box = ObjectBox().noteBox;
-
-              final List<Note> foundNotes = box
-                  .query(Note_.content.contains(searchQuery, caseSensitive: false))
-                  .build()
-                  .find();
-      
-              if (foundNotes.isNotEmpty) {
-                return SliverMasonryGrid.count(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 8,
-                  crossAxisSpacing: 8,
-                  childCount: foundNotes.length,
-                  itemBuilder: (context, index) {
-                    final note = foundNotes[index];
-                    return NoteItem(note: note);
-                  },
-                );
-              }
-              return _NotesEmptyIndicator();
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
