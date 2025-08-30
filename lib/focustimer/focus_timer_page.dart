e171eccdc19a25881f07abe5b0de8570e519043e
husen <iamhusen101@gmail.com>
2025-08-30 16:27:34 +0530

    chore: some structural ui changes

--- lib/focustimer/focus_timer_page.dart
+++ lib/focustimer/focus_timer_page.dart
@@ -12,7 +12,8 @@ class FocusTimerPage extends StatefulWidget {
   State<FocusTimerPage> createState() => _FocusTimerPageState();
 }
 
-class _FocusTimerPageState extends State<FocusTimerPage> with SingleTickerProviderStateMixin,AutomaticKeepAliveClientMixin{
+class _FocusTimerPageState extends State<FocusTimerPage>
+    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
   late TabController tabController;
 
   @override
@@ -21,74 +22,43 @@ class _FocusTimerPageState extends State<FocusTimerPage> with SingleTickerProvid
     tabController = TabController(length: 2, vsync: this);
     g.audioController.initialize();
   }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     final textTheme = context.textTheme;
-    final scheme = context.colorScheme;
     return Scaffold(
       appBar: AppBar(
         titleSpacing: 0,
         backgroundColor: getLerpedSurfaceColor(context),
-        title: Text('Focus', style: textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold)),
+        title: Text(
+          'Focus',
+          style: textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+        ),
         actions: [
-          InkWell(
-            child: Container(
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.all(0.9),
-              decoration: BoxDecoration(
-                color: scheme.primary.withValues(alpha: 0.3),
-                shape: BoxShape.circle,
-              ),
-              child: Icon(Icons.question_mark),
-            ),
-            onTap: () async {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(9),
-                    ),
-                    content: Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: const Text(
-                        "If notification permissions are granted, you can see persistent timer notification after enabling it from settings. Go to app's Settings > Timer Settings >Enable \"Show timer as notification in notification bar until ends.\"",
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          ),
+          _TimerNotificationInfoButton(),
         ],
       ),
       body: Column(
-          children: [
-            TabBar(
+        children: [
+          TabBar(
+            controller: tabController,
+            indicatorWeight: 3,
+            indicatorSize: TabBarIndicatorSize.tab,
+            labelStyle: const TextStyle(fontWeight: FontWeight.w600),
+            tabs: const [
+              Tab(text: 'Focus Timer'),
+              Tab(text: 'Relaxing Sounds'),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
               controller: tabController,
-              indicatorWeight: 3,
-              indicatorSize: TabBarIndicatorSize.tab,
-              labelStyle: const TextStyle(fontWeight: FontWeight.w600),
-              tabs: const [
-                Tab(text: 'Focus Timer'),
-                Tab(text: 'Relaxing Sounds'),
-              ],
+              children: [FocusTimerTab(), SoundTab()],
             ),
-            Expanded(
-              child: TabBarView(
-                controller: tabController,
-                children: [
-                  FocusTimerTab(),
-                  SoundTab()
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -97,3 +67,46 @@ class _FocusTimerPageState extends State<FocusTimerPage> with SingleTickerProvid
   bool get wantKeepAlive => true;
 }
 
+
+
+
+class _TimerNotificationInfoButton extends StatelessWidget {
+  const _TimerNotificationInfoButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final scheme = context.colorScheme;
+    return InkWell(
+      child: Container(
+        margin: EdgeInsets.all(8),
+        padding: EdgeInsets.all(0.9),
+        decoration: BoxDecoration(
+          color: scheme.primary.withValues(alpha: 0.3),
+          shape: BoxShape.circle,
+        ),
+        child: Icon(Icons.question_mark),
+      ),
+      onTap: () async {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+              content: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Text(
+                  "If notification permissions are granted, you can see persistent timer notification after enabling it from settings. Go to app's Settings > Timer Settings >Enable \"Show timer as notification in notification bar until ends.\"",
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
