e2ba91d049122cd4ba08f67bde333a399b41e0f7
husen <iamhusen101@gmail.com>
2025-08-31 22:32:28 +0530

    chore: move scrollable date bar to a separate file for reusability

--- lib/task/ui/tasks_page.dart
+++ lib/task/ui/tasks_page.dart
@@ -1,5 +1,6 @@
 import 'package:darrt/app/extensions/extensions.dart';
 import 'package:darrt/app/ui/empty_tasks_indicator.dart';
+import 'package:darrt/app/ui/widgets/scrollable_date_bar.dart';
 import 'package:darrt/helpers/globals.dart' as g;
 import 'package:darrt/helpers/messages.dart';
 import 'package:darrt/helpers/utils.dart';
@@ -8,7 +9,6 @@ import 'package:darrt/task/ui/task_delete_confirmation_dialog.dart';
 import 'package:darrt/task/ui/task_item.dart';
 import 'package:darrt/task/ui/task_timeline_item.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({super.key});
@@ -17,7 +17,8 @@ class TasksPage extends StatefulWidget {
   State<TasksPage> createState() => _TasksPageState();
 }
 
-class _TasksPageState extends State<TasksPage> with SingleTickerProviderStateMixin {
+class _TasksPageState extends State<TasksPage>
+    with SingleTickerProviderStateMixin {
   late final TabController _tabController;
 
   @override
@@ -34,7 +35,9 @@ class _TasksPageState extends State<TasksPage> with SingleTickerProviderStateMix
     return ListenableBuilder(
       listenable: Listenable.merge([g.taskVm, g.calMan]),
       builder: (context, child) {
-        final tasks = g.taskVm.tasks.where((t) => t.isActiveOn(g.calMan.selectedDate)).toList();
+        final tasks = g.taskVm.tasks
+            .where((t) => t.isActiveOn(g.calMan.selectedDate))
+            .toList();
         final textTheme = TextTheme.of(context);
         return NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled) => [
@@ -51,8 +54,12 @@ class _TasksPageState extends State<TasksPage> with SingleTickerProviderStateMix
                   controller: _tabController,
                   splashBorderRadius: BorderRadius.circular(10),
                   dividerHeight: 0,
-                  labelStyle: textTheme.titleSmall!.copyWith(fontWeight: FontWeight.bold),
-                  unselectedLabelStyle: textTheme.titleSmall!.copyWith(fontWeight: FontWeight.w500),
+                  labelStyle: textTheme.titleSmall!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  unselectedLabelStyle: textTheme.titleSmall!.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
                   tabs: const [
                     Tab(text: 'All'),
                     Tab(text: 'One-time'),
@@ -84,100 +91,9 @@ class _TasksPageState extends State<TasksPage> with SingleTickerProviderStateMix
   }
 }
 
-class DateItem extends StatelessWidget {
-  const DateItem({super.key, required this.date});
-
-  final DateTime date;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 1.0),
-      child: ListenableBuilder(
-        listenable: g.calMan,
-        builder: (context, child) {
-          final bool isSelected = g.calMan.selectedDate == date;
-          final bool isToday = DateUtils.isSameDay(date, DateTime.now());
-          final scheme = context.colorScheme;
-          final textTheme = context.textTheme;
-          // Determine text and background colors based on states
-          Color dayTextColor;
-          Color dateBackgroundColor;
-          Color dateTextColor;
-
-          if (isSelected) {
-            dayTextColor = scheme.primary;
-            dateBackgroundColor = scheme.primary;
-            dateTextColor = scheme.onPrimary;
-          } else if (isToday) {
-            dayTextColor = scheme.secondary;
-            dateBackgroundColor = scheme.secondaryContainer.withValues(alpha: 0.5);
-            dateTextColor = scheme.onSecondaryContainer;
-          } else {
-            dayTextColor = scheme.onSurface.withValues(alpha: 0.8);
-            dateBackgroundColor = Colors.transparent;
-            dateTextColor = scheme.onSurface;
-          }
-
-          return InkWell(
-            onTap: () => g.calMan.updateSelectedDate(date),
-            borderRadius: BorderRadius.circular(12),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                border: isToday
-                    ? Border.all(color: scheme.secondary.withValues(alpha: 0.5), width: 1.5)
-                    : null,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    DateFormat('EEE').format(date),
-                    style: textTheme.labelSmall?.copyWith(
-                      color: dayTextColor,
-                      fontWeight: isSelected || isToday ? FontWeight.w600 : FontWeight.normal,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Container(
-                    width: 26,
-                    height: 26,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: dateBackgroundColor,
-                      boxShadow: isSelected
-                          ? [
-                              BoxShadow(
-                                color: scheme.primary.withValues(alpha: 0.3),
-                                blurRadius: 4,
-                                offset: const Offset(0, 1),
-                              ),
-                            ]
-                          : null,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '${date.day}',
-                        style:textTheme.labelMedium?.copyWith(
-                          color: dateTextColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
 class TaskList extends StatefulWidget {
   const TaskList({super.key, required this.tasks, this.isRepeating});
+
   final List<Task> tasks;
   final bool? isRepeating;
 
@@ -185,7 +101,8 @@ class TaskList extends StatefulWidget {
   State<TaskList> createState() => _TaskListState();
 }
 
-class _TaskListState extends State<TaskList> with AutomaticKeepAliveClientMixin {
+class _TaskListState extends State<TaskList>
+    with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
 
@@ -218,7 +135,9 @@ class _TaskListState extends State<TaskList> with AutomaticKeepAliveClientMixin 
         // Filter tasks based on repeating status
         final filteredTasks = widget.isRepeating == null
             ? widget.tasks
-            : widget.tasks.where((t) => t.isRepeating == widget.isRepeating).toList();
+            : widget.tasks
+                  .where((t) => t.isRepeating == widget.isRepeating)
+                  .toList();
 
         if (filteredTasks.isEmpty) {
           return _getEmptyIndicator();
@@ -276,7 +195,8 @@ class TasksSelectedActions extends StatelessWidget {
               icon: CloseButtonIcon(),
             ),
             IconButton(
-              onPressed: () async => await _showTaskDeleteConfirmationDialog(context),
+              onPressed: () async =>
+                  await _showTaskDeleteConfirmationDialog(context),
               icon: Icon(Icons.delete),
             ),
           ],
@@ -295,36 +215,17 @@ class TasksSelectedActions extends StatelessWidget {
   }
 }
 
-
-
-class ScrollableDateBar extends StatelessWidget {
-  const ScrollableDateBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: g.calMan,
-      builder: (context, child) {
-        return ListView.builder(
-          controller: g.calMan.dateScrollController,
-          scrollDirection: Axis.horizontal,
-          itemExtent: g.calMan.dateItemWidth,
-          physics: const BouncingScrollPhysics(),
-          itemCount: g.calMan.dates.length,
-          itemBuilder: (context, index) => DateItem(date: g.calMan.dates[index]),
-        );
-      },
-    );
-  }
-}
-
 class _TabBarDelegate extends SliverPersistentHeaderDelegate {
   final TabBar _tabBar;
 
   _TabBarDelegate(this._tabBar);
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
     return Material(color: getSurfaceColor(context), child: _tabBar);
   }
 
@@ -343,10 +244,15 @@ class _TabBarDelegate extends SliverPersistentHeaderDelegate {
 
 class _TasksSelectedActionsDelegate extends SliverPersistentHeaderDelegate {
   final Widget _child;
+
   _TasksSelectedActionsDelegate(this._child);
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
     return Container(
       color: Theme.of(context).scaffoldBackgroundColor,
       height: kToolbarHeight,
