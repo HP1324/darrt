2c7fefd1165a629d5d54bd8e6231227e673bce79
husen <iamhusen101@gmail.com>
2025-09-01 14:29:51 +0530

    move initial scroll to date mechanism with postFrameCallback to new ScrollableDateBar, also late initialize dates

--- lib/app/state/managers/calendar_manager.dart
+++ lib/app/state/managers/calendar_manager.dart
@@ -1,16 +1,18 @@
+import 'package:darrt/app/extensions/extensions.dart';
+import 'package:darrt/helpers/utils.dart';
 import 'package:flutter/material.dart';
-import 'package:darrt/helpers/consts.dart';
 
 class CalendarManager extends ChangeNotifier {
   DateTime selectedDate = DateTime.now();
   DateTime previousSelectedDate = DateTime.now();
-  final maxDate = DateTime.now().add(Duration(days: maxExtentDateDays));
-  final initialDate = mInitialDate;
-  late List<DateTime> dates = List.generate(
-    maxDate.difference(initialDate).inDays + 1,
-        (index) => initialDate.add(Duration(days: index)),
-  );
+
+  final firstDate = getFirstDate();
+  final lastDate = getLastDate();
+
+  late final List<DateTime> dates;
+
   final dateItemWidth = 43.0;
+
   void updateSelectedDate(DateTime date) {
     previousSelectedDate = selectedDate;
     selectedDate = DateTime(date.year, date.month, date.day);
@@ -20,21 +22,24 @@ class CalendarManager extends ChangeNotifier {
   ScrollController dateScrollController = ScrollController();
 
   void scrollToDate(DateTime date) {
-    final index = dates.indexWhere((d) => DateUtils.isSameDay(d, date));
-    if(index != -1) {
-      dateScrollController.animateTo(index * dateItemWidth,
-          duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+    final index = dates.indexOf(date.dateOnly);
+    if (index != -1) {
+      dateScrollController.animateTo(
+        index * dateItemWidth,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
       updateSelectedDate(date);
     }
   }
 
   CalendarManager() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (dateScrollController.hasClients) {
-        scrollToDate(DateTime.now());
-      }
-    });
+    dates = List.generate(
+      lastDate.difference(firstDate).inDays + 1,
+      (index) => firstDate.add(Duration(days: index)),
+    );
   }
+
   @override
   void dispose() {
     dateScrollController.dispose();
