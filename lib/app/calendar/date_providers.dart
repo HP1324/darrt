0c11b6543885ffc21b18b8e87a4b007b7b8b3669
husen <iamhusen101@gmail.com>
2025-09-05 17:13:24 +0530

    migration: merge calendar folder from branch migration/riverpod

---
+++ lib/app/calendar/date_providers.dart
@@ -0,0 +1,53 @@
+import 'package:darrt/app/calendar/calendar_state.dart';
+import 'package:darrt/app/extensions/extensions.dart';
+import 'package:darrt/helpers/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'date_providers.g.dart';
+
+@riverpod
+class SelectedDateNotifier extends _$SelectedDateNotifier {
+
+  @override
+  CalendarState build() {
+    return CalendarState(
+      selectedDate: DateTime.now().dateOnly,
+      previousSelectedDate: DateTime.now().dateOnly,
+    );
+  }
+
+  void setSelectedDate(DateTime date) {
+    state = state.copyWith(previousSelectedDate: state.selectedDate);
+    state = state.copyWith(selectedDate: date.dateOnly);
+  }
+
+  void scrollToDate(DateTime date, [ScrollController? controller]) {
+    final dates = ref.read(datesProvider);
+    final index = dates.indexOf(date.dateOnly);
+    if (index != -1) {
+      // ignore: avoid_manual_providers_as_generated_provider_dependency
+      // final controller = ref.read(dateBarControllerProvider);
+      controller?.animateTo(
+        index * 43,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+      setSelectedDate(date);
+    }
+  }
+}
+
+
+@riverpod
+List<DateTime> dates(Ref ref) {
+  final firstDate = getFirstDate();
+  final lastDate = getLastDate();
+
+  return List.generate(
+    lastDate.difference(firstDate).inDays + 1,
+        (index) => firstDate.add(Duration(days: index)),
+  );
+}
+
---
+++ lib/app/calendar/date_providers.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'date_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$datesHash() => r'4c06aeb786da8f8a5a006cc45a7da93c318ae2a7';
+
+/// See also [dates].
+@ProviderFor(dates)
+final datesProvider = AutoDisposeProvider<List<DateTime>>.internal(
+  dates,
+  name: r'datesProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$datesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DatesRef = AutoDisposeProviderRef<List<DateTime>>;
+String _$selectedDateNotifierHash() =>
+    r'75d371637fc7927964bd5d4455c380ce3f28407e';
+
+/// See also [SelectedDateNotifier].
+@ProviderFor(SelectedDateNotifier)
+final selectedDateNotifierProvider =
+    AutoDisposeNotifierProvider<SelectedDateNotifier, CalendarState>.internal(
+      SelectedDateNotifier.new,
+      name: r'selectedDateNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$selectedDateNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SelectedDateNotifier = AutoDisposeNotifier<CalendarState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
